f85e887f3f0231ebde429c99033cc0416f260742
Thomas McDevitt <thomas.mcdevitt@pearson.com>
2020-03-09 13:46:42 +0000

    Basic boilerplate code in place for a pure front-end project.

---
+++ postcss.config.js
@@ -0,0 +1,10 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const ap = require('autoprefixer');
+
+const postcssConfig = {
+    plugins: [
+        ap
+    ]
+};
+
+module.exports = postcssConfig;
---
+++ src/main.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+
+// import App from './App';
+// import store from './store';
+
+import './style.scss';
+
+ReactDOM.render(
+    // <Provider store={store}>
+        <div className='doritieury' />,
+    // </Provider>,
+    document.getElementById('app')
+);

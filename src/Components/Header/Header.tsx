4262cc4fa0fd71379501d06300ab75d832680543
Tommy <rejectmusician@hotmail.co.uk>
2020-10-08 18:15:44 +0100

    Changes to header. - Added Amatic font. - Updated social media links. - Added responsive styling.

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -8,31 +8,38 @@ const Header: FunctionComponent = () => {
     <header className='header'>
       <div className='header__left'>
         <img src={MeImg} className='header__left__image' />
-        <div className='header__left__social'>
-          <div className='header__left__socialIcon header__left__socialIcon--facebook'>
+      </div>
+      <div className='header__right'>
+        <div className='header__right__keywordWrapper'>
+          <div className='header__right__keyword'>
+            engineer. gamer.
+          </div>
+          <div className='header__right__keyword'>
+            musician.
+          </div>
+        </div>
+        <div className='header__right__social'>
+          <div className='header__right__socialIcon header__right__socialIcon--facebook'>
             <a href='https://www.facebook.com/TMLKMCD/'>
               <i className='fab fa-facebook-square' />
             </a>
           </div>
-          <div className='header__left__socialIcon header__left__socialIcon--instagram'>
-            <a href='https://www.facebook.com/TMLKMCD/'>
+          <div className='header__right__socialIcon header__right__socialIcon--instagram'>
+            <a href='https://www.instagram.com/himynameistommy123/'>
               <i className='fab fa-instagram-square' />
             </a>
           </div>
-          <div className='header__left__socialIcon header__left__socialIcon--twitter'>
-            <a href='https://www.facebook.com/TMLKMCD/'>
+          <div className='header__right__socialIcon header__right__socialIcon--twitter'>
+            <a href='https://twitter.com/tmlkmcd'>
               <i className='fab fa-twitter-square' />
             </a>
           </div>
-          <div className='header__left__socialIcon header__left__socialIcon--linkedin'>
-            <a href='https://www.facebook.com/TMLKMCD/'>
+          <div className='header__right__socialIcon header__right__socialIcon--linkedin'>
+            <a href='https://www.linkedin.com/in/tmlkmcd/'>
               <i className='fab fa-linkedin' />
             </a>
           </div>
         </div>
-      </div>
-      <div className='header__right'>
-        hello!
       </div>
     </header>
   );
